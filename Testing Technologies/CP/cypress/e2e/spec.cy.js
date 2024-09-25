e3bc7ec75a5a8504e124aafdbd0698974edc9a7f
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-09-25 13:56:04 +0100

    Finished the first tasks for cypress

--- Testing Technologies/CP/cypress/e2e/spec.cy.js
+++ Testing Technologies/CP/cypress/e2e/spec.cy.js
@@ -1,3 +1,5 @@
+// First Cypress test - Open page, query element, click element and finally assert for information
+
 describe("Cypress example Test", () => {
   it("Visits the cypress kitchen sink, queries an element and interacts with it", () => {
     // Visit the page -
@@ -9,5 +11,39 @@ describe("Cypress example Test", () => {
     //  Assert the content on the page
     cy.url().should("include", "/commands/querying");
     cy.get("h1").should("contain", "Querying");
+  });
+});
+
+describe("Cypress Email example", () => {
+  it("Visit the commands actions site and clicks an element and inputs a value within", () => {
+    // Visit the page -
+    cy.visit("https://example.cypress.io/commands/actions");
+    // Query for an element
+    cy.get(".action-email");
+    // Type into the element
+    cy.get(".action-email").type("lewisgirvan@hotmail.com");
+    //  Assert the content on the page
+    cy.url().should("include", "/commands/actions");
+    cy.get(".action-email").should("have.value", "lewisgirvan@hotmail.com");
+  });
+});
+
+describe("Cypress Click on buttons example", () => {
+  it("Visit commands/actions, click multiple buttons inc different areas of the buttons", () => {
+    // Visit the page -
+    cy.visit("https://example.cypress.io/commands/actions");
+    // Query for an element
+    cy.get(".action-btn");
+    // Click on the element
+    cy.get(".action-btn").click();
+    // Query for an element
+    cy.get("#action-canvas");
+    // Click on the element
+    // Middle (Standard)
+    cy.get("#action-canvas").click();
+    // Top Left
+    cy.get("#action-canvas").click("topLeft");
+    // Bottom Right
+    cy.get("#action-canvas").click("bottomRight");
   });
 });
