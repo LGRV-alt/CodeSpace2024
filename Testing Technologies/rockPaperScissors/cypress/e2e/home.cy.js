c65639d1b2b25723705e9c963dbe0009a4ca0ff7
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-10-09 22:24:41 +0100

    adding tests to the page
    Testing the page elements, buttons and outputs

--- Testing Technologies/rockPaperScissors/cypress/e2e/home.cy.js
+++ Testing Technologies/rockPaperScissors/cypress/e2e/home.cy.js
@@ -1,7 +1,47 @@
+// Testing the Rock Paper Scissors Page
+
+// Check all the elements are present on the page
 describe("Home Page", () => {
-  it("The h1 Has the Title Rock Paper Scissors", () => {
+  it("Check all the elements are on the page", () => {
     cy.visit("http://127.0.0.1:5500/");
     cy.get("h1").contains("Rock Paper Scissors");
+    cy.get("h2").contains("Player Choice");
+    cy.get("h2").contains("Computer Option");
+    cy.get("h2").contains("Result");
+  });
+});
+
+// Test the rock button and the output matches
+describe("Rock Button", () => {
+  it("Click the rock button and expect the output to be rock", () => {
+    cy.visit("http://127.0.0.1:5500/");
     cy.get("button").contains("Rock").click();
+    cy.get("#user-option").contains("Rock");
+  });
+});
+
+// Test the Paper button and the output matches
+describe("Paper Button", () => {
+  it("Click the paper button and expect the output to be paper", () => {
+    cy.visit("http://127.0.0.1:5500/");
+    cy.get("button").contains("Paper").click();
+    cy.get("#user-option").contains("Paper");
+  });
+});
+
+// Test the Scissors button and the output matches
+describe("Scissors Button", () => {
+  it("Click the scissors button and expect the output to be scissors", () => {
+    cy.visit("http://127.0.0.1:5500/");
+    cy.get("button").contains("Scissors").click();
+    cy.get("#user-option").contains("Scissors");
+  });
+});
+
+describe("Lose?", () => {
+  it("Click the scissors button and expect the output to be scissors", () => {
+    cy.visit("http://127.0.0.1:5500/");
+    cy.get("button").contains("Scissors").click();
+    cy.get("#user-option").contains("Scissors");
   });
 });
