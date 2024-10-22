5c888070bed902752c10cc79b4c2d8130a9ca001
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-10-22 22:53:28 +0100

    adding admin tests

--- MK-Time Project/MK-Time/cypress/e2e/spec.cy.js
+++ MK-Time Project/MK-Time/cypress/e2e/spec.cy.js
@@ -8,6 +8,7 @@ describe("Test User Login Functions", () => {
     cy.get(".btn").click();
     cy.get("h2").should("contain", "Welcome to MK Time");
   });
+
   it("test adding product to the cart", () => {
     cy.get(
       ":nth-child(1) > .card > .list-group > .list-group-item.btn > .btn"
@@ -17,6 +18,7 @@ describe("Test User Login Functions", () => {
     cy.get("h2").should("contain", "Your Cart");
     cy.get(".ml-2 > .font-weight-bold").should("contain", "The Executive");
   });
+
   it("test removing item", () => {
     cy.get(
       ":nth-child(1) > .card > .list-group > .list-group-item.btn > .btn"
@@ -27,6 +29,7 @@ describe("Test User Login Functions", () => {
     cy.get(".align-items-center > .cart-delete-button").click();
     cy.get(".text-center").should("contain", "Your cart is empty.");
   });
+
   it("Test User logout", () => {
     cy.get(".navbar-brand").should("contain", "MK Time");
     cy.get(":nth-child(4) > .nav-link").click();
@@ -46,5 +49,42 @@ describe("Test User Login Functions", () => {
     cy.get(".card-title").should("contain", "The Heritage");
     cy.get('.card-body > form > [type="submit"]').click();
     cy.get(".cart-number").should("contain", 2);
+  });
+  it("Test the serach function", () => {
+    cy.get(".navbar-brand").should("contain", "MK Time");
+    cy.get(".form-control").type("brown");
+    cy.get(".form-inline > .btn").click();
+    cy.get(":nth-child(1) > .card > .card-body > .card-title").should(
+      "contain",
+      "The Voyager"
+    );
+    cy.get(".form-control").type("fluffy dog");
+    cy.get(".form-inline > .btn").click();
+    cy.get(
+      '[style="display: flex; justify-content: space-evenly; flex-wrap: wrap;"]'
+    ).should("contain", "0 records");
+  });
+});
+
+describe("Test admin functions", () => {
+  beforeEach(() => {
+    cy.visit(
+      "http://localhost/codeSpace-Challenges/CodeSpace-WebTech-Challenges/MK-Time/products.php"
+    );
+    cy.get("#inputEmail").click().type("Admin@mail.com");
+    cy.get("#inputPassword").click().type("Admin");
+    cy.get(".btn").click();
+    cy.get(".navbar-brand > span").should("contain", "Admin");
+  });
+  it("check product update", () => {
+    cy.get(
+      ":nth-child(1) > .card > .list-group > .list-group-item.btn > .btn"
+    ).click();
+    cy.get(".card-body > :nth-child(4)").click();
+    cy.get("h2").should("contain", "Update the product");
+  });
+  it("check the create function", () => {
+    cy.get(":nth-child(2) > .nav-link").click();
+    cy.get("h1").should("contain", "Add");
   });
 });
